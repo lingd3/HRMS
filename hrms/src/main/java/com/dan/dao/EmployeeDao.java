72e832f96d6931c0acb750b1a2e04d3c95a83d40
lingd3 <824928207@qq.com>
2017-11-27 16:28:59 +0800

    实现dao层

--- hrms/src/main/java/com/dan/dao/EmployeeDao.java
+++ hrms/src/main/java/com/dan/dao/EmployeeDao.java
@@ -1,5 +1,53 @@
 package com.dan.dao;
 
-public class EmployeeDao {
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.dan.domain.Employee;
+import com.dan.domain.Post;
+
+@Repository
+public interface EmployeeDao {
+	/**
+	 * 根据条件查询员工
+	 * 
+	 * @param map
+	 * @return
+	 */
+	public List<Post> findEmployees(Map<String, Object> map);
+
+	/**
+	 * 根据条件查询员工数量
+	 * 
+	 * @param map
+	 * @return
+	 */
+	public Integer getCount(Map<String, Object> map);
+
+	/**
+	 * 添加员工
+	 * 
+	 * @param employee
+	 * @return
+	 */
+	public Integer addEmployee(Employee employee);
+
+	/**
+	 * 修改员工
+	 * 
+	 * @param employee
+	 * @return
+	 */
+	public Integer updateEmployee(Employee employee);
+
+	/**
+	 * 删除员工
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Integer deleteEmployee(String id);
 
 }
