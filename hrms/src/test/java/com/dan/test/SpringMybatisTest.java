48a4efd7f1b49f1a4cccb877aea8d7735ac07dc4
lingd3 <824928207@qq.com>
2017-11-28 16:29:36 +0800

    springmvc与mybatis整合

--- hrms/src/test/java/com/dan/test/SpringMybatisTest.java
+++ hrms/src/test/java/com/dan/test/SpringMybatisTest.java
@@ -1,5 +1,33 @@
 package com.dan.test;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.dan.domain.Admin;
+import com.dan.service.AdminService;
+
+/**
+ * 配置spring和junit整合，junit启动时加载springIOC容器 spring-test,junit
+ * @author gd
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+// 告诉junit spring 配置文件
+@ContextConfiguration({ "classpath:spring-mybatis.xml" })
 public class SpringMybatisTest {
 
+    @Autowired
+    private AdminService adminService;
+
+    @Test
+    public void testLogin() {
+        Admin admin = new Admin();
+        admin.setUsername("superadmin");
+        admin.setPassword("123456");
+        System.out.println(adminService.login(admin).toString());
+
+    }
 }
